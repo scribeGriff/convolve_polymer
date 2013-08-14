08f7575f748b53cff9c7598146961888bd9ce0ea
Richard Griffith <scribe.griff@gmail.com>
2013-08-13 21:19:50 -0500

    mathjax issue initializing inside template tag

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Inital Experiments with Polymer ##
 
-Experiment 1: Get MathJax rendering to work.
+Experiment 1: Get MathJax rendering to work.   
+
+- MathJax rendering seems to have a problem with the template tag.  Trying a similar experiment with AngularDart to see if I can get any further.
+- Observation: Both polymer and angular seem to exhibit an odd UX behavior when moving the cursor back to correct a bound input.  After each keystroke, the cursor jumps to the end of the already entered text regardless of whether the user is finished correcting the earlier text, which is extremely undesirable. 
--- web/convolve_polymer.dart
+++ web/convolve_polymer.dart
@@ -1,6 +1,23 @@
 import 'dart:html';
-//import 'package:polymer/polymer.dart';
+import 'package:polymer/polymer.dart';
+import 'package:fancy_syntax/syntax.dart';
+import 'package:js/js.dart' as js;
+
+class Math extends Object with ObservableMixin {
+  @observable
+  String message;
+}
+
+String render(String v) {
+  js.context.MathJax.Hub.Typeset();
+  return v;
+}
 
 main() {
-  query('#tmpl').model = r'$$f(x) = 2 + 5x^{-1} - 3x^{-2} + 7x^{-3}$$';
+  var globals = {
+                 'render': (String v) => render(v)
+  };
+  var equation = query('#tmpl');
+  equation.bindingDelegate = new FancySyntax(globals: globals);
+  equation.model = new Math();
 }
