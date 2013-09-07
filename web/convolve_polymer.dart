163a5032e90950c56646a9bcdd897fee3d7998ed
Richard Griffith <scribe.griff@gmail.com>
2013-09-06 21:01:08 -0500

    messy, but more tests rendering polynomials

--- web/convolve_polymer.dart
+++ web/convolve_polymer.dart
@@ -4,34 +4,44 @@ import 'package:convolab/convolab.dart';
 import 'package:js/js.dart' as js;
 
 class Math extends Object with ObservableMixin {
-  @observable String expression;
-  Math([this.expression = '']);
+  @observable String numerator, denominator;
+  Math([this.numerator = '', this.denominator]);
 }
 
 @CustomTag('equation-element')
 class Equations extends PolymerElement with ObservableMixin {
-  String equation;
+  String ncoeff, dcoeff;
   @observable Math math = new Math();
 
   // This is not in the shadow dom since MathJax
   // does not typset inside a polymer-element.
-  DivElement eqndiv = query('#equation');
+  DivElement numeratordiv = query('#numerator');
+  DivElement denominatordiv = query('#denominator');
+  DivElement solutiondiv = query('#solution');
+  DivElement resultsdiv = query('#results');
 
   bool get applyAuthorStyles => true;
 
   void render(Event e, var detail, Node target) {
-    equation = math.expression;
-    Sequence coeff = sequence(equation.split(",")
+    ncoeff = math.numerator;
+    dcoeff = math.denominator;
+    Sequence numeqn = sequence(ncoeff.split(",")
         .where((element) => element.trim().isNotEmpty)
           .map((element)=> int.parse(element)));
-    eqndiv.innerHtml = pstring(coeff);
+    Sequence deneqn = sequence(dcoeff.split(",")
+        .where((element) => element.trim().isNotEmpty)
+          .map((element)=> int.parse(element)));
+    var convolution = conv(numeqn, deneqn);
+    numeratordiv.innerHtml = pstring(numeqn);
+    denominatordiv.innerHtml = pstring(deneqn);
+    solutiondiv.innerHtml = convolution.format();
     var context = js.context;
     js.scoped(() {
       // This repesents the following:
-      // MathJax.Hub.Queue(["Typeset", MathJax.Hub, eqndiv]));
+      // MathJax.Hub.Queue(["Typeset", MathJax.Hub, numeratordiv]));
       new js.Proxy(context.MathJax.Hub.Queue(js.array(["Typeset",
                                                        context.MathJax.Hub,
-                                                       eqndiv])));
+                                                       resultsdiv])));
     });
   }
 }
