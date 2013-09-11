adf7823f3ee78d43ed928bfc9da9806ecc2a6da3
Richard Griffith <scribe.griff@gmail.com>
2013-09-10 21:26:03 -0500

    navbar is now repeating template

--- web/elements/navbar_element.dart
+++ web/elements/navbar_element.dart
@@ -1,34 +1,30 @@
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 
+/// The Item class represents an item in the navigation bar.
+class Item extends Object with ObservableMixin {
+  @observable String text;
+  @observable String id;
+
+  Item([this.text = '', this.id = '']);
+}
+
+/// The navigation bar.
 @CustomTag('navbar-element')
-class NavbarElement extends PolymerElement {
+class NavbarElement extends PolymerElement with ObservableMixin {
+  final ObservableList<Item> menu =
+      toObservable([
+                    new Item('Convolution', 'convolution'),
+                    new Item('Deconvolution', 'deconvolution'),
+                    new Item('Sequences', 'sequence'),
+                    new Item('Filters', 'filter'),
+                    new Item('Fourier', 'fourier')
+                    ]);
 
   bool get applyAuthorStyles => true;
 
-  // Do I really want to do it like this?
-  void doConvolution(Event e, var detail, Node target) {
+  void menuHandler(Event e, var detail, Element target) {
     e.preventDefault();
-    print('selected convolution');
-  }
-
-  void doDeconvolution(Event e, var detail, Node target) {
-    e.preventDefault();
-    print('selected deconvolution');
-  }
-
-  void doSequences(Event e, var detail, Node target) {
-    e.preventDefault();
-    print('selected sequences');
-  }
-
-  void doFilters(Event e, var detail, Node target) {
-    e.preventDefault();
-    print('selected filters');
-  }
-
-  void doFourier(Event e, var detail, Node target) {
-    e.preventDefault();
-    print('selected fourier');
+    print(target.attributes["id"]);
   }
 }
