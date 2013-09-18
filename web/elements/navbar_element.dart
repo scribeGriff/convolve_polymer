befa6e82754405fa72bb3c7aee135e47f05250ee
Richard Griffith <scribe.griff@gmail.com>
2013-09-17 21:54:20 -0500

    manipulating classes

--- web/elements/navbar_element.dart
+++ web/elements/navbar_element.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:collection';
 import 'package:polymer/polymer.dart';
 //import 'package:animation/animation.dart';
 
@@ -32,23 +33,54 @@ class NavbarElement extends PolymerElement with ObservableMixin {
                     new Item('Filters', 'filter', false)
                     ]);
 
+  // The default selection is convolution which is the 2nd entry in menu.
+  int currentSelection = 2;
+  int newSelection;
+  int offsetClass;
+  ListQueue classList = new ListQueue();
+
+  void NavbarElement() {
+  classList.addAll([
+                    "previous-previous",
+                    "previous",
+                    "active",
+                    "next",
+                    "next-next"
+                    ]);
+  }
+
   bool get applyAuthorStyles => true;
 
   void menuHandler(Event e, var detail, Element target) {
     e.preventDefault();
     String selected = target.attributes["id"];
     //print(target.attributes["id"]);
+    // There's got to be an easier way to find selected id.
     menu.forEach((element) {
       if (element.id == selected) {
+        newSelection = menu.indexOf(element);
+        offsetClass = newSelection - currentSelection;
+        print(menu.indexOf(element));
         element.selected = true;
-        query("#$selected").classes.removeAll(["previous", "next"]);
-        query("#$selected").classes.add("active");
-      } else {
-        element.selected = false;
-        query("#convolution").classes.remove("active");
-        query("#convolution").classes.add("previous");
+        currentSelection = newSelection;
       }
     });
+    // Now need to offset classList queue by the offsetClass amount.
+    // Do that here using pop/push where sign determines direction.
+    // Once you have that, loop through menu item list and attach the correct class.
+    // Something like this?
+    for (var i = 0; i < menu.length; i++) {
+      query("#${menu[i].id}").classes.removeAll(classList);
+      query("#${menu[i].id}").classes.add(classList.elementAt(i));
+    }
+//        query("#$selected").classes.removeAll(classList);
+//        query("#$selected").classes.add("active");
+//      } else {
+//        element.selected = false;
+//        query("#convolution").classes.remove("active");
+//        query("#convolution").classes.add("previous");
+//      }
+
 
     /*var el = query('#box');
 
